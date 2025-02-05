b914814a977f377e9c08f27280c9f101298ab55c
Ethan Tang <itsethang5@gmail.com>
2025-02-05 15:26:10 -0500

    Add cart button to nav bar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,12 @@
 import { Flex } from "@radix-ui/themes";
 import UserOptions from "./UserOptions";
+import Cart from "./Cart";
 
 const Navbar = () => {
   return (
-    <Flex className="bg-blue-400 h-20 px-5" align="center" justify="between">
+    <Flex className="bg-blue-400 h-15 px-7" align="center" justify="between">
       <UserOptions />
+      <Cart />
     </Flex>
   );
 };
