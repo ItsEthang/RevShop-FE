4c93919dab9dae9da624bd34271f9d2d1d23a499
Ethan Tang <itsethang5@gmail.com>
2025-02-05 16:15:16 -0500

    Set up React Query

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,18 @@ import "./index.css";
 import App from "./App.tsx";
 import { Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Theme accentColor="yellow" grayColor="slate">
-      <App />
-    </Theme>
+    <QueryClientProvider client={queryClient}>
+      <Theme accentColor="yellow" grayColor="slate">
+        <App />
+        <ReactQueryDevtools />
+      </Theme>
+    </QueryClientProvider>
   </StrictMode>
 );
