8ff3a20c5fd438483559737488500b465a864472
Ethan Tang <itsethang5@gmail.com>
2025-02-05 13:48:03 -0500

    Create the product page layout

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,11 +1,14 @@
+import { Box } from "@radix-ui/themes";
+
 const ProductPage = () => {
   return (
-    <div className="grid grid-flow-col grid-rows-3 gap-4 h-full">
-      {" "}
-      <div className="row-span-3 bg-red-200">Menu</div>{" "}
-      <div className="col-span-2 bg-blue-200">Navbar</div>{" "}
-      <div className="col-span-2 row-span-2 bg-black">Content</div>
-    </div>
+    <>
+      <Box className="h-screen w-50 fixed bg-red-200">Menu</Box>
+      <Box className="ml-50">
+        <Box className="bg-blue-200 h-20">Navbar</Box>{" "}
+        <Box className="bg-black">Content</Box>
+      </Box>
+    </>
   );
 };
 
