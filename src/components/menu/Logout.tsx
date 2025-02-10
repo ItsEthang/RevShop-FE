707885ff88158ff7501a84335ee4276bc861310d
Ethan Tang <itsethang5@gmail.com>
2025-02-09 23:56:36 -0500

    Implement logout functionality

--- src/components/menu/Logout.tsx
+++ src/components/menu/Logout.tsx
@@ -1,9 +1,11 @@
 import { Button, Text } from "@radix-ui/themes";
+import useSignOut from "react-auth-kit/hooks/useSignOut";
 import { FiLogOut } from "react-icons/fi";
 
 const Logout = () => {
+  const signOut = useSignOut();
   return (
-    <Button color="ruby">
+    <Button color="ruby" onClick={() => signOut()}>
       <Text weight="bold" as="span">
         Logout
       </Text>
